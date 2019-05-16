58bb0a656e01f06f0ce44dd2a04c0037c555f61a
aminekhalef <46798295+aminekhalef@users.noreply.github.com>
2019-05-17 00:36:07 +0200

    Update create_long.m

--- MA_Amine_Kh/create_long.m
+++ MA_Amine_Kh/create_long.m
@@ -2,53 +2,58 @@
 % according to the model
 function M=create_long(M,time_thr)
 
- 
+
 m=0;
 ind=0;
-indarr=[];
-%-----------------
-while ind<size(M,2)  %&& ~check_time_thr_all(M(4,:),time_thr)
+indarr=[]; % array containing the indexes of Gear shifting or unknown maneuver
+
+while ind<size(M,2)
     
-    
-    ind=ind+1;
-    
-    if check_time_thr(M(:,ind), time_thr)
-        k=ind+1;
-        while k< size(M,2) && check_time_thr(M(:,k), time_thr)
+    ind=ind +1 ;
+    k=ind+1;
+    if k >=size(M,2)
+        break;
+    end
+    if check_time(M(:,k), time_thr)
+        
+        % while GS or unknown maneuver add the duration and performed
+        % displacement
+        while k<= size(M,2) && check_time(M(:,k), time_thr)
             
             m=m+1;
             indarr(m)=k;
+            M(3,ind)=M(3,k);
             M(4,ind)=M(4,k)+M(4,ind);
             M(5,ind)=M(5,k)+M(5,ind);
             k=k+1;
             
-            
         end
-        M(3,ind)=M(3,k);
-        M(4,ind)=M(4,k)+M(4,ind);
-        M(5,ind)=M(5,k)+M(5,ind);
-        M(end,ind)=M(end,k); % label the new curve segment
-        m=m+1;
-        indarr(m)=k;
+        % concatenate to the next available defined maneuver (acc or dec or static cruising)
+%         M(4,ind)=M(4,k-1)+M(4,ind);
+%         M(5,ind)=M(5,k-1)+M(5,ind);
+%         M(end,ind)=M(end,k); % label the new curve segment
+%         m=m+1;
+%         indarr(m)=k;
         
         if ~isempty(indarr)
             
-            M(:,indarr)=[];
+            M(:,indarr)=[]; % remove unknown maneuver segments (columns in the matrix)
         end
         %             j=1;
-        m=0;
-        indarr=[];
-        
+        m=0; % preallocate for the next iteration
+        indarr=[];% preallocate for the next iteration
+%         ind = k;
     end
-
+    
 end
- 
+
+k=0;
 label=M(end,:);
 m=0;
 ind=0;
 indarr=[];
 %-------------------
-while  ind<length(label) && ~check_adj(label) % && 
+while  ind<length(label) && ~check_adj(label) % &&
     
     ind=ind+1;
     k=ind;
@@ -95,7 +100,7 @@ while  ind<length(label) && ~check_adj(label) % &&
         indarr=[];
         
         
-    else
+    elseif label(k)==0
         j=k+1;
         
         while j<=size(M,2) && label(j)==0
@@ -116,34 +121,50 @@ while  ind<length(label) && ~check_adj(label) % &&
         j=1;
         m=0;
         indarr=[];
+    elseif label(k)==-99
+        j=k+1;
         
+        while j<=size(M,2) && label(j)==-99
+            
+            m=m+1;
+            indarr(m)=j;
+            M(3,k)=M(3,j);
+            M(4,k)=M(4,k)+M(4,j);
+            M(5,k)=M(5,k)+M(5,j);
+            j=j+1;
+        end
+        if ~isempty(indarr)
+            
+            M(:,indarr)=[];
+        end
+        
+        j=1;
+        
+        m=0;
+        indarr=[];
     end
-   label=M(end,:);
- 
+    label=M(end,:);
     
 end
+
 end
- 
- 
- 
-function b=check_time_thr(vec,time_thr)
- 
+
+
+%--- subfunctions
+% this function checks if a certain threshold is exceeded
+function b=check_time(vec,time_thr)
+
 b=false;
 
 if  vec(4)<(time_thr)
     b=true;
     
 end
- 
- 
 end
 
-
-function b=check_adj(vec) % check whether an array contains duplicate consecutive elements   
+% this function checks whether two consecutive array elements are duplicate
+% and will be needed in the final labeling
+function b=check_adj(vec) %
 temp=unique(vec,'stable');
 b=isequal(temp,vec);
- 
 end
- 
- 
- 
