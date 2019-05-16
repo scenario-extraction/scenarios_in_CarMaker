6332551e39518862ff5a359bfb11172a3fa83f90
aminekhalef <46798295+aminekhalef@users.noreply.github.com>
2019-05-17 00:40:36 +0200

    update the automation of the tensors' creation

--- MA_Amine_Kh/main.m
+++ MA_Amine_Kh/main.m
@@ -1,12 +1,11 @@
 % main script to run : edited to process erg files from Testrun series with variations!
 % make the necessary changes, e.g path, directory ...
-% the simulations output files are now saved in the default directory
-% automate the creation of model tensor respectively for every Testrun
+% the simulations' output erg files are now saved in the default directory
 
 %% preprocess
 clc
 close all
-clear all
+% clear all
 
 %% add necessaray dir to search path
 dir_TR = 'F:\Highway_scenarios_overwrite\Data\TestRun';
@@ -14,27 +13,34 @@ maindir = 'F:\Highway_scenarios_overwrite\SimOutput';
 
 date_str= datestr(now,'yyyymmdd');
 
-dir2ergfile =strcat('F:\Highway_scenarios_overwrite\SimOutput\itiv-3039\',date_str); % to be adapted to today's date!!
+dir2ergfile =strcat('F:\Highway_scenarios_overwrite\SimOutput\itiv-3039\',date_str); %
 
 dire2cmenv = 'F:\Highway_scenarios_overwrite\src_cm4sl';
 addpath(dir2ergfile,maindir, dire2cmenv,dir_TR);
 
 %% set Matlab env to use Matlab utilities for CM. Path directory must contain cmenv()
 cmenv();
+% preallocate
+TestRun=[];
 
 %% run the simulations and process the erg files
 % list erg files from first simulation
 cd (dir2ergfile);
-TestRun.GT = dir('*.erg');
+TestRun.first_sim = dir('*.erg');
+disp(length(TestRun.first_sim));
 
+% define indicator for resimulation
+global flag_resim;
 
-flag_sim = 0;
-ind_file =0;
-if ~isempty(TestRun.GT)
+ind_file =0; % loop index
 
-    for k=1:length(TestRun.GT)
+for k=1:length(TestRun.first_sim)
+    if ~isempty(TestRun.first_sim)
+        
+        flag_resim =0;
+        
         %% set the path for the current Testrun
-        path2ergfile = fullfile(dir2ergfile, TestRun.GT(k).name);
+        path2ergfile = fullfile(dir2ergfile, TestRun.first_sim(k).name);
         
         %% create struct containing needed data from CM
         prepdata_filename = datapreparation(path2ergfile);
@@ -44,51 +50,63 @@ if ~isempty(TestRun.GT)
         
         %% Create the tensor describing the scenario based on the data from CM and the prediction models
         
-        [Tensor_CM, Tensor_model] = create_tensor(prepdata_filename,label_data, flag_sim);
+        [Tensor_CM, Tensor_model] = create_tensor(prepdata_filename,label_data);
         
         %% save output Tensors in the corresponding Testrun struct
-        TestRun.GT(k).Tensor_CM = Tensor_CM;
-        TestRun.GT(k).Tensor_model = Tensor_model;
+        TestRun.first_sim(k).Tensor_CM = Tensor_CM;
+        TestRun.first_sim(k).Tensor_model = Tensor_model;
         %% overwrite the infofile for resimulation
-        % file_to_overwrite = 'Highway_with_data_export_ov';
+%         file_to_overwrite = 'ChangingLanes_ov';
+%         file_to_overwrite = 'Highway_with_data_export_ov';
         file_to_overwrite= 'Ground_Truth_label_Highwat_01_ov';
         overwrite_infofile(Tensor_CM,file_to_overwrite);
         %% save
-        
         %% delete
         delete(prepdata_filename,label_data);
         
         %% run resimulation via channel communication
         run_sim_via_ch_com(file_to_overwrite);
-       
+        
         %% define the pattern of the erg file from overwritten infofiles get erg files from the resimulation
         Files_ov = dir(fullfile(dir2ergfile, '*_ov_*'));
+        temp_anz = numel(Files_ov);
         for i=1:numel(Files_ov)
             
             [~,~,ext] = fileparts(Files_ov(i).name);
             if strcmp(ext,'.erg')
                 ind_file= ind_file+1;
-               TestRun.resim(ind_file+1).data = Files_ov(i);
+                TestRun.resim(ind_file).data=Files_ov(i);
             end
-             
+            
         end
+        %reallocate
+        Files_ov =[];
         
-        %delete current resimulation info & erg files and prepare for next resimulation
-        delete(fullfile(dir2ergfile, '*_ov_*'))
+        %% create tensors for resimulation
+        flag_resim =1;
+        temp_path = fullfile(TestRun.resim(ind_file).data.folder,TestRun.resim(ind_file).data.name);
+        prepdata_filename_resim = datapreparation(temp_path);
         
-        %% to edit and workaround !
-%         Files_ov = [];
-%         TestRun.resim(ind_file+1) = Files_ov;
-%          prepdata_filename = datapreparation(dir2ergfile);
-%         
-%         label_data = labeling(prepdata_filename);
-%         
-%         Tensor_resim = create_tensor(prepdata_filename,label_data);
+        label_data_resim = labeling(prepdata_filename_resim);
         
-    end
-else
-    disp('No erg files was found');
-end
+        [Tensor_resim_CM, Tensor_resim_model] = create_tensor_resim(prepdata_filename_resim,label_data_resim,k);
+        
+        % store output tensors from resimulation
+        TestRun.resim(ind_file).data.Tensor_CM = Tensor_resim_CM;
+        TestRun.resim(ind_file).data.Tensor_model = Tensor_resim_model;
+        
+        % delete
+        delete(prepdata_filename_resim,label_data_resim);
+        delete('Ego_plot.fig');
+        delete('Ego_plot_final.fig');
 
+        %delete the info & erg files for current resimulation and prepare for next resimulation
+        delete(fullfile(dir2ergfile, '*_ov_*'))
+        pause(1);
+        
+    else
+        disp('No erg files were found');
+    end
+end
 
 
