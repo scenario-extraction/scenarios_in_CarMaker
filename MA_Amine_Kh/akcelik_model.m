a7374a3021613f84a436f27e0c2527e9e9069d81
aminekhalef <46798295+aminekhalef@users.noreply.github.com>
2019-08-20 23:18:00 +0200

    Update akcelik_model.m

--- MA_Amine_Kh/akcelik_model.m
+++ MA_Amine_Kh/akcelik_model.m
@@ -1,15 +1,20 @@
-% predict velocity based on Akcelik  Model 
+% predict velocity based on Akcelik Model 
 function [vel,m,B] =  akcelik_model(v_i,v_f,t_a,t_arr, ru)
 
+t_a = round(t_a,2);
 a_avg= (v_f-v_i)/t_a; % calculate average acceleration
 
 m = (15-27*ru+sqrt(81*ru*ru-138*ru+73))/(12*ru-4); % calculate calibration parameter m
 
 B = (2*(m+1)*(m+2)*a_avg)/(m^2);% model parameter
 
+
 acc = arrayfun(@(x) B*(x/t_a)*(1-(x/t_a)^m)^2,t_arr);% acc profile prediction
-
+if (isnan(acc(1)))
+    acc(1)=acc(2);
+end
+    
 % velocity prediction: integration of the predicted acceleration
-vel = cumtrapz(t_arr,acc)+v_i; 
 
+vel =cumtrapz(t_arr,acc)+v_i;
 end
