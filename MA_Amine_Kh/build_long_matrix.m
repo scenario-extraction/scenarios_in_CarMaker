61931f8e69c02ceacf07e15152c00166db0f62c6
aminekhalef <46798295+aminekhalef@users.noreply.github.com>
2019-08-20 23:22:44 +0200

    Update build_long_matrix.m

--- MA_Amine_Kh/build_long_matrix.m
+++ MA_Amine_Kh/build_long_matrix.m
@@ -5,25 +5,24 @@
 
 %---Abbreviations:
 %GS: gear shifting
-function M = build_long_matrix(Ego_Car_ax, Ego_Car_vx, Time, Ego_sRoad, Time_thr)
+function M = build_long_matrix(Ego_Car_ax, Ego_Car_vx, Time, Ego_sRoad)
 
-M = []; % preallocate
+% preallocate
+M = [];
 
-Ego_Car_ax_thr = 0; % threshold of the acceleration
+Ego_Car_ax_thr = 0.25; % threshold of the acceleration
+Time_thr =1.0; % time threshold corresponding to the duration of GS
 
 %% The section of the function partitions the acc-velocity curve based on the elementar longitudinal maneuvers: acceleration, deceleration and constant velocity cruising
 % M: is the output matrix of this function described linewise as described in the model:
 %1. beginning Time
-%2. initial velocity in mps
-%3. final velocity in mps
+%2. initial velocity
+%3. final velocity
 %4. duration in s
 %5. displacement in m
 %6. label
 
-% the matlab predifined function round is used in this case to get consistent
-% partitioning of the acceleration-velocity curve. In order to work with plausibel data, very small values of
-% acceleration will be considered as zeros. 
-
+% loop indexes
 j=0;
 m=0;
 
@@ -38,10 +37,10 @@ while j<length(Time)
         k=j-1;
         
     end
-       
-    if round(Ego_Car_ax(j),1)>Ego_Car_ax_thr
+    
+    if (Ego_Car_ax(j))>Ego_Car_ax_thr
         
-        while  j<length(Time) && round(Ego_Car_ax(j),1)>Ego_Car_ax_thr
+        while  j<length(Time) && (Ego_Car_ax(j))>Ego_Car_ax_thr
             
             j=j+1;
             
@@ -50,9 +49,9 @@ while j<length(Time)
         m=m+1;
         M(:,m)=[Time(k);Ego_Car_vx(k);Ego_Car_vx(j);Time(j)-Time(k);Ego_sRoad(j)-Ego_sRoad(k);1];% add the new motion segment to M
         
-    elseif round(Ego_Car_ax(j),1)<Ego_Car_ax_thr
+    elseif (Ego_Car_ax(j))<-Ego_Car_ax_thr
         
-        while  j<length(Time) && round(Ego_Car_ax(j),1)<Ego_Car_ax_thr
+        while  j<length(Time) && (Ego_Car_ax(j))<-Ego_Car_ax_thr
             
             j=j+1;
             
@@ -62,7 +61,7 @@ while j<length(Time)
         M(:,m)=[Time(k);Ego_Car_vx(k);Ego_Car_vx(j);Time(j)-Time(k);Ego_sRoad(j)-Ego_sRoad(k);-1];% add the new motion segment to M
         
     else
-        while j<length(Time) && round(Ego_Car_ax(j),1)==Ego_Car_ax_thr
+        while j<length(Time) && abs(Ego_Car_ax(j))<=Ego_Car_ax_thr
             
             j=j+1;
             
@@ -74,7 +73,6 @@ while j<length(Time)
 end
 
 %% create final version of the matrix
-
 M=create_long(M,Time_thr);
 
 end
