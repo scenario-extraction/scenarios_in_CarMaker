7859c6d61522f86a962b6ceff19a9645ab415a1d
aminekhalef <46798295+aminekhalef@users.noreply.github.com>
2019-08-20 23:24:35 +0200

    add documentation

--- MA_Amine_Kh/check_detectable.m
+++ MA_Amine_Kh/check_detectable.m
@@ -1,8 +1,14 @@
-function b = check_detectable(vec)  % function to check wether an Obj is detectable or not 
+% function to check wether an Obj is detectable or not
+% vec ist the the array vector of the detection levels
+function b = check_detectable(vec)
 
 b=true;
 binc = [0,1,2]; % detection Levels
 counts = hist(vec,binc);
+
+% time threshold is 250 stamps = 5 seconds due to sample time of 0.02 s.
+%TObj detection's duration below 5s will be igonred. Threshold can be
+%adapted
 if counts(1)>=length(vec)-250
     b=false;
 end
